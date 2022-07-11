e13da3df3d9253f544944fa2219fab7c3902ad9c
Mario Dubreuil <mario_dubreuil@hotmail.com>
2022-07-10 20:15:49 -0400

    add sensor controller and update nuget package

---
+++ WeatherApiProducer/Controllers/SensorController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherApiProducer.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SensorController : ControllerBase
+{
+    private readonly ILogger<SensorController> _logger;
+
+    public SensorController(ILogger<SensorController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetMessage")]
+    public string Get()
+    {
+        return "testing: 1, 2!";
+    }
+}
+
